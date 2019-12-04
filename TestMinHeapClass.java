b253def25347d77262e00283b8bac7eb562e2bd5
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-12-04 19:46:17 +1000

    Немного тестов

--- TestMinHeapClass.java
+++ TestMinHeapClass.java
@@ -1,29 +1,25 @@
 class TestMinHeapClass{
 	public static void main(String[] args){
 		MinHeap minHeap = new MinHeap();
-		minHeap.add(10);
-		System.out.println("1 " + minHeap.getMin());
-		minHeap.add(20);
-		minHeap.add(100);
-		System.out.println("2 " + minHeap.getMin());
+		minHeap.add(3);
+		minHeap.add(2);
+		minHeap.add(1);
 		minHeap.add(0);
-		System.out.println("3 " + minHeap.getMin());
-		minHeap.add(-123);
-		System.out.println("4 " + minHeap.getMin());
-		minHeap.popMin();
-		System.out.println("5 " + minHeap.getMin());
-		/*minHeap.add(0);
 		minHeap.add(0);
-		System.out.println("6 " + minHeap.size());
-		
-		minHeap.println();
-		MinHeap minHeap2 = new MinHeap(minHeap);
-		minHeap.clear();
-		
-		System.out.println("7 " + minHeap.empty());
-		System.out.println("8 " + minHeap.size());
+		minHeap.add(0);
 
-		System.out.println("9 " + minHeap2.empty());
-		System.out.println("10 " + minHeap2.size());*/
+		MinHeap minHeap2 = new MinHeap(minHeap);
+		int i = 1;
+		while (!minHeap.empty()){
+			System.out.println(i++ + "| " + minHeap.get());
+			minHeap.pop();	
+		}
+
+		i = 1;
+		while (!minHeap2.empty()){
+			System.out.println(i++ + "| " + minHeap2.get());
+			minHeap2.pop();	
+		}
+		
 	}
 }
